7395a4fb84bf3a578040d0c380b57aceceb3e529
Brad McDanel <mcdabw6@gmail.com>
2022-04-17 15:38:40 -0400

    fixed split on space

--- vmcalc/01/src/main.c
+++ vmcalc/01/src/main.c
@@ -1,53 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *read_file(char *filename) {
-  FILE *fp = fopen(filename, "r");
-  if (fp == NULL) {
-    printf("Error opening file\n");
-    exit(1);
-  }
-
-  fseek(fp, 0, SEEK_END);
-  long fsize = ftell(fp);
-  fseek(fp, 0, SEEK_SET);
-
-  char *string = malloc(fsize + 1);
-  fread(string, fsize, 1, fp);
-  fclose(fp);
-
-  string[fsize] = 0;
-  return string;
-}
-
-char *skip_space(char *str) {
-  while (*str == ' ' || *str == '\t') {
-    str++;
-  }
-  return str;
-}
-
-char *next_word(char *str) {
-  if (*str == '\0') {
-    return NULL;
-  }
-
-  char *word = skip_space(str);
-  while (*str != ' ' && *str != '\0' && *str != '\n') {
-    str++;
-  }
-
-  str++;
-  return str;
-}
+#include "io.h"
 
 int main(int argc, char *argv[]) {
   char *filename = argv[1];
   char *str = read_file(filename);
-  char *word = next_word(str);
-  while (*word != '\0') {
+  char *word = next_word(&str);
+  while (word != NULL) {
     printf("%s\n", word);
-    word = next_word(word);
+    word = next_word(&str);
   }
+
   return 0;
 }
