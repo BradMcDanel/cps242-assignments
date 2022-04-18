f8c037cfdb30a6bfd64c9dc8addbec8fc374a201
Brad McDanel <bmcdanel@fandm.edu>
2022-04-18 10:04:21 -0400

    Finished setup for first vm step.

--- vmcalc/01/include/io.h
+++ vmcalc/01/include/io.h
@@ -3,9 +3,12 @@
 
 #include <stdbool.h>
 
-char* read_file(char* filename);
-char* next_word(char** str);
-bool is_number(char* str);
-bool is_alphanumeric(char* str);
+char *read_file(char *filename);
+char *next_word(char **str);
+bool str_equals(char *str1, char *str2);
+bool str_in_list(char *str, char **list);
+bool is_keyword(char *str, char **keywords);
+bool is_number(char *str);
+bool is_variable(char *str);
 
 #endif // _IO_H_
--- vmcalc/01/src/main.c
+++ vmcalc/01/src/main.c
@@ -1,33 +1,79 @@
+#include <inttypes.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <inttypes.h>
 
 #include "io.h"
 
 #define MAX_PROGRAM_SIZE 2048
+#define MAX_CONSTANT_SIZE 256
 
 typedef enum {
-  OP_ADD
-} Opcode;
+  OP_PRINT = 1,
+  OP_CONSTANT = 2,
+} OpCode;
+
+static char *keywords[] = {
+    "print",
+    NULL,
+};
+
+typedef struct {
+  int constant;
+} Value;
 
 typedef struct {
   uint8_t code[MAX_PROGRAM_SIZE];
+  int code_length;
+  Value constants[MAX_CONSTANT_SIZE];
+  int constant_length;
 } Program;
 
-Program program = {0};
-int program_counter = 0;
+typedef struct {
+  uint16_t pc;
 
-int main(int argc, char *argv[]) {
-  char *filename = argv[1];
+} VM;
+
+void add_code(Program *program, uint8_t code) {
+  program->code[program->code_length] = code;
+  program->code_length++;
+}
+
+Program generate_program(char *filename) {
+  Program program = {0};
   char *str = read_file(filename);
   char *word = next_word(&str);
   while (word != NULL) {
+    // I have provided some helper functions to check if a word is:
+    //   - a number (e.g. "123")
+    //   - a variable (e.g. "_x2321")
+    //   - a keyword (e.g. "print")
     bool is_num = is_number(word);
-    bool is_alphanum = is_alphanumeric(word);
-    printf("%s (%d/%d)\n", word, is_num, is_alphanum);
+    bool is_var = is_variable(word);
+    bool is_kw = is_keyword(word, keywords);
+    printf("%s\t(%d/%d/%d)\n", word, is_num, is_var, is_kw);
+
+    // TODO: Add OP_PRINT and OP_CONSTANT instructions to the program.
+    //     : For OP_CONSTANT, add the constant to the program's constants array.
+
+    // This advances to the next word (don't remove!)
     word = next_word(&str);
   }
 
+  return program;
+}
+
+void run_program(Program *program) {
+  VM vm = {0};
+  for (vm.pc = 0; vm.pc < program->code_length; vm.pc++) {
+    uint8_t code = program->code[vm.pc];
+    // TODO: implement OP_PRINT and OP_CONSTANT here
+  }
+}
+
+int main(int argc, char *argv[]) {
+  char *filename = argv[1];
+  Program program = generate_program(filename);
+  run_program(&program);
   return 0;
 }
